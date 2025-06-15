29eff073e1563d18022ef51388fa0cc9373ae902
swarnakar06 <aswarnakar_be22@thapar.edu>
2025-06-15 06:36:34 +0530

    Configure static file settings for Render

--- tpn_calculator/settings.py
+++ tpn_calculator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tpn-calculator.onrender.com']
+ALLOWED_HOSTS = ['tpn-calculator.onrender.com', 'localhost', '127.0.0.1']
 
 
 
